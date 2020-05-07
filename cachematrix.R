2d18c16e698162649b36113e3a6e7915366a7b46
SAS01913959154 <duttasejuti24@gmail.com>
2020-05-07 19:51:04 +0600

    comitting the assignment
    modifying the R script with necessary requirements.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+##this fuction caclculate an inverse of a matrix and retrive the result from cache
+##this assignment gives us a rough illustration of how lexical scoping works
 
-## Write a short comment describing this function
+##the first makeCacheMatrix stores a matrix and its inverse
+#it used set and get fuction to set values and retrive values
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()){
+  i<-NULL
+  set<-function(mat){
+    x<<-mat
+    i<<-NULL
+  }
+  get<-function() x
+  setInv<-function(inv)
+    i<-inv
+  getInv<-function() i
+  list(set=set,get=get,setInv=setInv,getInv=getInv)
 }
 
+##without this second function the previous function is incomplete because this function
+##actually calculate the inverse
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve<-function(x,...){
+  i<-x$getInv()
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data<-x$get()
+  if(length(data)%%sqrt(length(data))==0)
+   i<-solve(data,...)
+  x$setInv(i)
+  i
+  }
